4bcec1761af176f9fef181167fce8142442f99b8
lucas.j <lucas.j@hpcnt.com>
2021-09-15 20:53:10 +0900

    setup project with webpack and some packages for this prjects

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # indexeddb
 Do you know how to use indexeddb?
+
+# Setup
+1. make common javascript browser setup and open with live-server
+2. open chrome and chrome devtool.
+3. go to application and looking for indexedDB
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+
+const mode = process.env.NODE_ENV === "production" ? "production" : "development"
+module.exports = {
+  mode,
+  entry:{
+    main:"./src/js/app.js"
+  },
+  output:{
+    filename:"[name].js",
+    path:path.resolve("./build")
+  },
+  module:{
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template:"./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  devServer: {
+    open: true,
+  },
+}
